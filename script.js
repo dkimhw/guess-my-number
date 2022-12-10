fc5f8e8732548908853a35ac21f1d1781173fa24
David Kim <dkhw90@gmail.com>
2022-12-10 23:32:32 +0100

    Wrap up game logic; add start a new game logic

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🥳 Correct Number!';
 
+const mysteryNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 20;
 
+document.querySelector('.start').addEventListener('click', () => {
+  score = 20;
+  document.querySelector('.check').disabled = false;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
+  mysteryNumber = Math.trunc(Math.random() * 100) + 1;
+});
+
+// document.querySelector('.number').textContent = mysteryNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No Number!';
+  } else if (guess === mysteryNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number!';
+  } else if (guess > mysteryNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < mysteryNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+
+  if (score <= 0) {
+    alert('You lost the game!');
+    document.querySelector('.check').disabled = true;
+  }
+
+  console.log(score);
 });
