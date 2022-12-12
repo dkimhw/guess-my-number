fdaffc401f7a55ec1903106943f2b37f7566f39c
David Kim <dkhw90@gmail.com>
2022-12-12 01:24:36 +0100

    Wrap up highscore feature

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 
 const mysteryNumber = Math.trunc(Math.random() * 100) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.start').addEventListener('click', () => {
   score = 20;
   document.querySelector('.check').disabled = false;
   document.querySelector('.score').textContent = 20;
   document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
   mysteryNumber = Math.trunc(Math.random() * 100) + 1;
 });
 
@@ -20,6 +24,12 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = '🛑 No Number!';
   } else if (guess === mysteryNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!';
+
+    // When player wins
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = Math.max(score, highscore);
+
   } else if (guess > mysteryNumber) {
     document.querySelector('.message').textContent = '📈 Too High!';
     score--;
@@ -31,9 +41,10 @@ document.querySelector('.check').addEventListener('click', () => {
   }
 
   if (score <= 0) {
-    alert('You lost the game!');
+    document.querySelector('.message').textContent = '💥 You lost the game!'
     document.querySelector('.check').disabled = true;
-  }
 
-  console.log(score);
+    // When player loses
+    document.querySelector('body').style.backgroundColor = '#FF5733';
+  }
 });
